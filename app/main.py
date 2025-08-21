bdc5c5bfd0b4b9668a63d60fc4771c01b17b8823
norlandrhagen <norlandrhagen@gmail.com>
2025-08-21 15:49:42 -0600

    adding RPS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-# import logfire
+import logfire
 from mangum import Mangum
 
 from app import xpublish_app
 
 rest = xpublish_app()
 handler = Mangum(rest.app)
-# logfire.instrument_aws_lambda(handler)
+logfire.instrument_aws_lambda(handler)
