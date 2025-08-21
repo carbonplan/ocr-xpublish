38cf5077f6295d4a058c692794e20cad801b3c5c
norlandrhagen <norlandrhagen@gmail.com>
2025-08-21 15:32:41 -0600

    wip

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ import os
 
 os.environ["NUMBA_DISABLE_JIT"] = "1"  # fixes threading errors
 
-import logfire
+# import logfire
 import xarray as xr
 import xpublish
 from fastapi.middleware.cors import CORSMiddleware
@@ -16,30 +16,48 @@ def apply_time_horizon(ds: xr.Dataset, var: str) -> xr.Dataset:
     return ds
 
 
+def get_rps_ds():
+    import icechunk
+    import xarray as xr 
+    storage = icechunk.s3_storage(
+        bucket="carbonplan-ocr",
+        prefix=f"input/fire-risk/tensor/USFS/RDS-2022-0016-02_EPSG_4326_icechunk_all_vars",
+        region="us-west-2",
+        anonymous=True,
+    )
+    repo = icechunk.Repository.open(storage)
+    session = repo.readonly_session("main")
+    ds = xr.open_zarr(session.store, consolidated=False)[['RPS']]
+    for var in list(ds):
+        # logfire.info(f"Applying time horizon to variable: {var}")
+        ds = apply_time_horizon(ds, var)
+
+    return ds
+
 def get_ds(branch: str):
-    with logfire.span(f"Loading dataset for branch: {branch}"):
-        import icechunk
-        import xarray as xr
+    # with logfire.span(f"Loading dataset for branch: {branch}"):
+    import icechunk
+    import xarray as xr
 
-        with logfire.span("opening icechunk repository"):
-            storage = icechunk.s3_storage(
-                bucket="carbonplan-ocr",
-                prefix=f"output/fire-risk/tensor/{branch}/template.icechunk",
-                region="us-west-2",
-                anonymous=True,
-            )
-            repo = icechunk.Repository.open_or_create(storage)
-            session = repo.readonly_session("main")
+    # with logfire.span("opening icechunk repository"):
+    storage = icechunk.s3_storage(
+        bucket="carbonplan-ocr",
+        prefix=f"output/fire-risk/tensor/{branch}/template.icechunk",
+        region="us-west-2",
+        anonymous=True,
+    )
+    repo = icechunk.Repository.open(storage)
+    session = repo.readonly_session("main")
 
-            with logfire.span("opening xarray dataset from icechunk repository"):
-                ds = xr.open_zarr(session.store, consolidated=False)
+        # with logfire.span("opening xarray dataset from icechunk repository"):
+    ds = xr.open_zarr(session.store, consolidated=False)
 
-                with logfire.span("applying time horizons"):
-                    for var in list(ds):
-                        logfire.info(f"Applying time horizon to variable: {var}")
-                        ds = apply_time_horizon(ds, var)
+            # with logfire.span("applying time horizons"):
+    for var in list(ds):
+        # logfire.info(f"Applying time horizon to variable: {var}")
+        ds = apply_time_horizon(ds, var)
 
-                    return ds
+    return ds
 
 
 def request_attributes_mapper(request, attributes):
@@ -58,22 +76,21 @@ def request_attributes_mapper(request, attributes):
         }
         # return None
 
-
 def xpublish_app():
-    logfire.configure()
-    logfire.info("Starting xpublish app...")
-    logfire.instrument_requests()
-    logfire.instrument_system_metrics()
+    # logfire.configure()
+    # logfire.info("Starting xpublish app...")
+    # logfire.instrument_requests()
+    # logfire.instrument_system_metrics()
 
     rest = xpublish.Rest(
-        {"prod": get_ds(branch="prod"), "QA": get_ds(branch="QA")},
+        {"prod": get_ds(branch="prod"), "QA": get_ds(branch="QA"),"RPS": get_rps_ds()},
         plugins={"wms": CfWmsPlugin()},
         cache_kws=dict(available_bytes=1e9),
     )
 
-    logfire.instrument_fastapi(
-        rest.app, request_attributes_mapper=request_attributes_mapper
-    )
+    # logfire.instrument_fastapi(
+    #     rest.app, request_attributes_mapper=request_attributes_mapper
+    # )
 
     # allow cors for carbonplan.org, localhost, and vercel
     rest.app.add_middleware(
