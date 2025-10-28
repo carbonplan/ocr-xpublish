4a3354b384ef26a7dbad06dfec8ab36a997c97af
norlandrhagen <norlandrhagen@gmail.com>
2025-10-28 00:34:13 -0600

    nit

--- app/serve.py
+++ app/serve.py
@@ -1,13 +1,10 @@
 from app import xpublish_app
-import os
-
-os.environ["XPUBLISH_TILES_LOG_LEVEL"] = "debug"
 
 
 def main():
     rest = xpublish_app()
 
-    rest.serve(port=9000, log_level="debug")
+    rest.serve()
 
 
 if __name__ == "__main__":
